dc2eed57333267ca57e7a43a3854c48f23ef995c
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-03-16 20:56:22 +0530

    Remove semicolon

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,8 @@ import Dashboard from '@/components/Dashboard'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(Router);
-Vue.use(BootstrapVue);
+Vue.use(Router)
+Vue.use(BootstrapVue)
 
 export default new Router({
   routes: [
