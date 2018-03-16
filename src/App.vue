bd9e642a031121124fddc42bce17fb986e96101c
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-03-16 20:56:50 +0530

    Comment margin-top and text allign

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
+    
     <router-view/>
   </div>
 </template>
@@ -16,8 +17,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* text-align: center; */
+  color: #6a8096;
+  /* margin-top: 60px; */
 }
 </style>
