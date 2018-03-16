f7ee26a946ee69c93e0d65a82baba714993bd584
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-03-16 12:09:53 +0530

    Add dashboard route

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Dashboard from '@/components/Dashboard'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(Router)
+Vue.use(Router);
+Vue.use(BootstrapVue);
 
 export default new Router({
   routes: [
@@ -10,6 +15,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard
     }
   ]
 })
