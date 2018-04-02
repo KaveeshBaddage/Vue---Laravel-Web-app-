53a8d42534400d9b9b7af267e2b51cb7a59441c6
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-04-02 07:21:07 +0530

    Create footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-footer app>
+          <v-spacer></v-spacer>
+          &copy;{{copywrites}}
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    copywrites: 'Kaveesh Baddage'
+  }
+  )
+}
+</script>
+
+<style scoped>
+</style>
